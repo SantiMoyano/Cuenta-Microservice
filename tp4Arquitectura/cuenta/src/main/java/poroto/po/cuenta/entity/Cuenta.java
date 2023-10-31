a2ebdac15ec55a97c76aacd5a077efbab08a16d0
Santiago Moyano <santisebmoyanosuarez@gmail.com>
2023-10-30 21:25:35 -0300

    agrega cuenta request

--- tp4Arquitectura/cuenta/src/main/java/poroto/po/cuenta/entity/Cuenta.java
+++ tp4Arquitectura/cuenta/src/main/java/poroto/po/cuenta/entity/Cuenta.java
@@ -1,9 +1,12 @@
 package poroto.po.cuenta.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Cuenta {
@@ -15,6 +18,11 @@ public class Cuenta {
     private Float saldo;
     private String fecha_alta;
     private boolean habilitada;
+
+//  meter entity de Usuario en la carpeta una vez terminado 
+//  @ManyToMany
+//  private List<Usuario> usuarios;
+    
     public Cuenta() {
     }
     public Cuenta(Integer mercado_pago, Float saldo, String fecha_alta) {
@@ -23,37 +31,44 @@ public class Cuenta {
         this.fecha_alta = fecha_alta;
         this.habilitada=true;
     }
+
     public Long getId_cuenta() {
         return id_cuenta;
     }
+
     public void setId_cuenta(Long id_cuenta) {
         this.id_cuenta = id_cuenta;
     }
+
     public Integer getMercado_pago() {
         return mercado_pago;
     }
+
     public void setMercado_pago(Integer mercado_pago) {
         this.mercado_pago = mercado_pago;
     }
+
     public Float getSaldo() {
         return saldo;
     }
+
     public void setSaldo(Float saldo) {
         this.saldo = saldo;
     }
+
     public String getFecha_alta() {
         return fecha_alta;
     }
+
     public void setFecha_alta(String fecha_alta) {
         this.fecha_alta = fecha_alta;
     }
+
     public boolean isHabilitada() {
         return habilitada;
     }
+
     public void setHabilitada(boolean habilitada) {
         this.habilitada = habilitada;
     }
-
-    
-    
 }
